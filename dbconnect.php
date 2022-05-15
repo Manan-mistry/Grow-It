28bef6a7ac850c24b3ae0c728ab4938f55154ff0
Manan-mistry <84577614+Manan-mistry@users.noreply.github.com>
2022-05-15 18:04:01 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 $servername="localhost";
 $username="root";
 $password="";
-$dbname="sn";
+$dbname="growit.sql";
 $conn=mysqli_connect($servername,$username,$password,$dbname);
 if(!$conn){
 		die("Error while connecting...");
